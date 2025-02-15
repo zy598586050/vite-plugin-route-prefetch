85997564a91332c02912a1e7ec451045639aaf1a
zhangyu <598586050@qq.com>
2025-02-15 16:53:28 +0800

    feat: types类型问题

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import routePrefetch from '../src/index'
+// import routePrefetch from '../src/index'
+import routePrefetch from 'vite-plugin-route-prefetch'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [routePrefetch(),vue()],
+  plugins: [
+    routePrefetch(),
+    vue()
+  ],
 })
