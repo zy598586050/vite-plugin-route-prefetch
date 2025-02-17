5f4326c57a4e6f631b8f2b246209ab77cf2db2e9
zhangyu <598586050@qq.com>
2025-02-17 23:55:37 +0800

    types: 类型修改

---
+++ rollup.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import typescript from 'rollup-plugin-typescript2'
+import terser from '@rollup/plugin-terser'
+import del from 'rollup-plugin-delete'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: 'src/index.ts',
+            output: [
+                {
+                    file: 'dist/index.cjs',
+                    format: 'cjs'
+                },
+                {
+                    file: 'dist/index.mjs',
+                    format: 'es'
+                }
+            ],
+            external: ['vite'],
+            plugins: [
+                del({ targets: 'dist' }),
+                typescript({
+                    tsconfig: 'tsconfig.json',
+                    useTsconfigDeclarationDir: true
+                }),
+                terser()
+            ]
+        }
+    }
+})
